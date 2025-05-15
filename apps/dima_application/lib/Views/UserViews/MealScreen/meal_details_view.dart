4937ccdcc699f307d3140686bf95087273cdfc98
Jacopo <jacopo@ideologica.it>
2025-05-15 14:09:42 +0200

    [Frontend] Remove wrong parameter

--- apps/dima_application/lib/Views/UserViews/MealScreen/meal_details_view.dart
+++ apps/dima_application/lib/Views/UserViews/MealScreen/meal_details_view.dart
@@ -238,7 +238,7 @@ final localizations = AppLocalizations.of(context)!;
                 // Use the existing provider to toggle meal status
                 ref
                     .read(todayPageProvider.notifier)
-                    .toggleMealCompletion(meal.name, mealPlanId);
+                    .toggleMealCompletion(meal.name);
               },
               icon: Icon(
                 isMealCompleted ? Icons.check_circle : Icons.circle_outlined,
